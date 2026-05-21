d2b9d9edec39e82dbb93a92b215fbe15d418a9eb
imSalwa <sallwaalbazei@gmail.com>
2026-05-21 14:15:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         SRMS mySystem = new SRMS();
-
-
+        
         List<Student> savedData = StudentFileManager.loadStudents();
         mySystem.getStudents().addAll(savedData);
-      
+        
         AutoSaveThread autoSave = new AutoSaveThread(mySystem);
         autoSave.start();
 
         System.out.println("Student Record Management System Started");
         System.out.println("[System] Current Database Records:");
-      
         mySystem.displayAllStudents();
         mySystem.displayStudentsByGpa(3.5);
         mySystem.displayStudentsByDepartment("IT");
