28c31305e23337eb3471cac8159affc1fef5cc05
imSalwa <sallwaalbazei@gmail.com>
2026-05-21 13:18:37 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SRMS mySystem = new SRMS();
+
+
+        List<Student> savedData = StudentFileManager.loadStudents();
+        mySystem.getStudents().addAll(savedData);
+      
+        AutoSaveThread autoSave = new AutoSaveThread(mySystem);
+        autoSave.start();
+
+        System.out.println("Student Record Management System Started");
+        System.out.println("[System] Current Database Records:");
+      
+        mySystem.displayAllStudents();
+        mySystem.displayStudentsByGpa(3.5);
+        mySystem.displayStudentsByDepartment("IT");
+        mySystem.displayStudentsByYear(2026);
+        
+        System.out.println("Testing Complete. Auto-Save Active in Background.");
+    }
+}
