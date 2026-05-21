c1de292a24f506adfaad8234fec4c42cb3f8d62c
imSalwa <sallwaalbazei@gmail.com>
2026-05-21 14:41:16 +0300

    Create AutoSaveThread.java

---
+++ AutoSaveThread.java
@@ -0,0 +1,25 @@
+public class AutoSaveThread extends Thread {
+    private SRMS system;
+
+
+    public AutoSaveThread(SRMS system) {
+        this.system = system;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+              
+                Thread.sleep(30000);
+                
+                
+                StudentFileManager.saveStudents(system.getStudents());
+                System.out.println("\n[Auto-Save] Database backup updated successfully.");
+                
+            } catch (InterruptedException e) {
+                System.out.println("Auto-save thread interrupted: " + e.getMessage());
+                break; 
+            }
+        }
+    }
