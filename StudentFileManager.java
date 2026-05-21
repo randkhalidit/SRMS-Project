5d08b4e9275661141222e99e3ca77df748e915a7
imSalwa <sallwaalbazei@gmail.com>
2026-05-21 14:40:28 +0300

    Create StudentFileManager.java

---
+++ StudentFileManager.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentFileManager {
+    private static final String FILE_NAME = "students.txt";
+
+   
+    public static void saveStudents(List<Student> students) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
+            for (Student s : students) {
+               
+                writer.println(s.getId() + "," + s.getName() + "," + s.getGpa() + "," + s.getDepartment() + "," + s.getYear());
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving to file: " + e.getMessage());
+        }
+    }
+
+    public static List<Student> loadStudents() {
+        List<Student> students = new ArrayList<>();
+        File file = new File(FILE_NAME);
+        
+        if (!file.exists()) return students; 
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 5) {
+                    int id = Integer.parseInt(parts[0]);
+                    String name = parts[1];
+                    double gpa = Double.parseDouble(parts[2]);
+                    String dept = parts[3];
+                    int year = Integer.parseInt(parts[4]);
+                    
+                    students.add(new Student(name, id, gpa, dept, year));
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error loading file data: " + e.getMessage());
+        }
+        return students;
+    }
+}
